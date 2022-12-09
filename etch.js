13e25923c94a54e62f2ce8da77b92b6bc7264296
Anantha Medu <ananthamedu18@gmail.com>
2022-12-10 00:02:20 +0530

    Nothing

--- etch.js
+++ etch.js
@@ -1,27 +1,55 @@
-const CONTAINER = document.getElementById("container");
+const container = document.getElementById("container");
+
+let bgroundColor;
 
 
 function createGrid(n)
 {
     for(let i=0; i<(n*n); i++)
+    {
+        const pixel = document.createElement("div");
+        pixel.classList.add('pixel');
+        container.appendChild(pixel);
+        let cellDimension = (container.clientHeight - (2*n))/n;
+        pixel.style.width = `${cellDimension}px`;
+        pixel.style.height = `${cellDimension}px`;
+    }
+
+    const square = document.querySelectorAll('.pixel');
+    console.log(square);
+    square.forEach(cell => cell.addEventListener("mouseover", changeBackgroundOnHover()));
+}
+
+function clearGrid()
 {
-    const pixel = document.createElement("div");
-    pixel.classList.add('pixel');
-    CONTAINER.appendChild(pixel);
-    let cellDimension = (CONTAINER.clientHeight - (2*n))/n;
-    pixel.style.width = `${cellDimension}px`;
-    pixel.style.height = `${cellDimension}px`;
-}
+    while(container.hasChildNodes())
+    {
+        container.removeChild(container.firstChild);
+    }
 }
 
-createGrid(10);
 
-const CELL = document.querySelectorAll('.pixel');
-console.log(CELL);
-CELL.forEach(cell => cell.addEventListener("mouseover", changeBackgroundOnHover));
+const slide = document.getElementById('slide'),
+    sliderDiv = document.getElementById("sliderAmount");
+
+
+slide.onchange = function() {
+    sliderDiv.innerHTML = this.value;
+    clearGrid();
+    createGrid(+this.value);
+}
+
+// const square = document.querySelectorAll('.pixel');
+// console.log(square);
+// Array.from.square.forEach(cell => cell.addEventListener("mouseover", changeBackgroundOnHover()));
+
+// document.body.addEventListener( 'mouseover', function ( event ) {
+//     if( event.target.classList == 'pixel' ) {
+//       changeBackgroundOnHover();
+//     };
+//   } );
 
 function changeBackgroundOnHover(e)
 {
     e.target.style.backgroundColor = "red";
 }
-
